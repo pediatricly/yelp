bb664b1361c3dc24e4727cf7b0c4e4ba1a559b86
pediatricly <mdscahill@gmail.com>
2016-08-15 11:00:36 -0700

    fixed error created from unicode to ascii search results. Ran search for SJ campbell dinner.

--- api1.py
+++ api1.py
@@ -1,6 +1,18 @@
 #! /usr/bin/python
 
 '''
+How I've been using:
+- Manual search in Yelp for the area, filtering & skimming for key parameters.
+- Type the restaurant names into the searches list & update city
+- Run the program to extract search data
+- Do diligence from the resulting sheet
+
+It would be nice to get rid of those manual steps. Could certainly do this -
+there is a fragment loop to go through all the hits that are returned from a
+general search in a certain geo area. Haven't implemented yet as there is still
+that need to filter with human insight.
+
+
 Documentation on this:
 The Business API
     https://www.yelp.com/developers/documentation/v2/business
@@ -21,7 +33,11 @@ import string
 import json
 import io
 
-searches = ['Marlowe', 'Fly Trap', 'the-cavalier']
+searches = ['Bar Bay Grill', 'Naschmarkt', 'South Winchester BBQ',
+            'Orchard City Kitchen', 'Kaizen Japanese Bar', 'The Table',
+            'Pacific Catch', 'Black Sheep Brasserie', 'Fogo de Chao Brazilian',
+            'Siena Bistro', 'Willard Hicks', 'Dry Creek Grill', 'LB Steak']
+city = '95128'
 
 '''
             ['1760',
@@ -64,28 +80,38 @@ for searchName in searches:
     }
 
     # results = {
-    res = client.search('San Francisco', **params)
-    topHit = res.businesses[0]
-    biz = topHit
-    # res = client.get_business('Bobo-san-francisco', **params)
-    # for biz in res.businesses:
     term = params['term']
-    phone = str(biz.phone)
-    phone = phone[:3] + '.' + phone[3:6] + '.' + phone[6:]
-    url = biz.url
-    url = string.split(url, '?')[0]
-    rating = biz.rating
+    name = ''
+    phone = ''
+    url = ''
+    rating = ''
     address = ''
-    for i, line in enumerate(biz.location.display_address):
-        if i == 1: pass
-        elif i == 1: address += line[:-1] + ' '
-        else: address += line + ' '
-    neighborhood = biz.location.neighborhoods[0]
-    name = biz.name
+    neighborhood = ''
+    try:
+        res = client.search(city, **params)
+        topHit = res.businesses[0]
+        biz = topHit
+        # res = client.get_business('Bobo-san-francisco', **params)
+        # for biz in res.businesses:
 
+        phone = str(biz.phone)
+        phone = phone[:3] + '.' + phone[3:6] + '.' + phone[6:]
+        url = biz.url
+        url = string.split(url, '?')[0]
+        rating = biz.rating
+        for i, line in enumerate(biz.location.display_address):
+            if i == 1: pass
+            elif i == 1: address += line[:-1] + ' '
+            else: address += line + ' '
+        neighborhood = biz.location.neighborhoods[0]
+        name = biz.name
+
+    except: pass
     result = [term, name, url, phone, address, neighborhood, rating]
-
     for field in result:
+        if type(field) == unicode:
+            field = field.encode('utf8')
         field = string.replace(str(field), ',', '')
         outfile.write(field + ',')
     outfile.write('\n')
+
